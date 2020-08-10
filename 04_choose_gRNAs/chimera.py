ef17d5e2963dab06775fec2010562a76bf9f4573
Cory Weller <cory.a.weller@gmail.com>
2020-08-10 14:57:15 -0400

    linting

--- 04_choose_gRNAs/chimera.py
+++ 04_choose_gRNAs/chimera.py
@@ -3,6 +3,8 @@
 def read_text(filename):
     with open(filename, 'r') as infile:
         rawText = infile.read()
+    if debug:
+        print("Read in text:\n" + rawText)
     return(rawText)
 
 def unfold_alignment(rawText):
@@ -17,29 +19,29 @@ def unfold_alignment(rawText):
     for line in text:
         if lineCount %4 == 0:
             seq1add = line.split()[1]
-            lineLen = len(seq1add)
+            line_len = len(seq1add)
             seq1 += seq1add
         elif lineCount %4 == 1:
             seq2add = line.split()[1]
-            lineLen = len(seq2add)
+            line_len = len(seq2add)
             seq2 += seq2add
         elif lineCount %4 == 2:
             alnAdd = line.lstrip()
-            alnPadding = lineLen - len(alnAdd)
+            alnPadding = line_len - len(alnAdd)
             alnScore += ' ' * alnPadding + alnAdd
         elif lineCount %4 == 3:
             pass
         lineCount += 1
     # modify score string
-    newScore = ''
+    new_score = ''
     for aa1, aa2, score in zip(seq1, seq2,alnScore):
         if aa1 == "-" and aa2 != "-":
-            newScore += 's'
+            new_score += 's'
         elif aa1 != "-" and aa2 == "-":
-            newScore += 'f'
+            new_score += 'f'
         else:
-            newScore += score
-    return([seq1, seq2, newScore])
+            new_score += score
+    return([seq1, seq2, new_score])
 
 
 def get_homology_regions(aln_scores, n_perfect_matches):
@@ -79,7 +81,7 @@ def pad_from_homology(non_homology_regions, nt_padding):
 def open_fasta(filename):
     with open(filename, "r") as infile:
         fasta_text = infile.readlines()
-        return(fasta_text)
+        return fasta_text
 
 def get_index_offset(seq):
     seq_offset = []
@@ -90,13 +92,13 @@ def get_index_offset(seq):
             seq_offset.append(index_value)
         else:
             seq_offset.append("NA")
-    return(seq_offset)
+    return seq_offset
 
-def returnCombos(dict1, dict2, ranges):
+def return_combos(dict1, dict2, ranges):
     for i in ranges:
         for start in range(i[0], i[1]):
             for end in range(start, i[1]):
-                yield((d1[start],d2[end]))
+                yield(dict1[start], dict2[end])
 
 def permute_alignment(alignment, n):
     alignment_list = list(alignment)
@@ -114,16 +116,16 @@ def permute_peptide_fasta(fasta_text):
     random.shuffle(seq)
     return(header + '\n' + wrap_fasta(''.join(seq) + '*') + '\n')
 
-def check_positive_integer(value):
-    try:
-        ivalue = int(value)
-    except TypeError:
-        raise argparse.ArgumentTypeError
-    if int(value) <= 0:
-        raise argparse.ArgumentValueError("%s is not a positive integer" % value)
-    return ivalue
 
-def main():
+
+def run_clustal(b_alignment):
+    output = subprocess.run(["./clustalo", "-i", "-", "--outfmt", "clu"], input=b_alignment, stdout=PIPE).stdout.decode()
+    if debug:
+        print(output)
+    return output
+
+if __name__ == "__main__":
+    print("main!")
     import os
     import sys
     import re
@@ -135,38 +137,41 @@ def main():
 
     parser = argparse.ArgumentParser()
     parser.add_argument('pep1_filename', metavar='pep1.fasta', type=str,
-                    help='First peptide fasta file')
+                        help='First peptide fasta file')
 
     parser.add_argument('pep2_filename', metavar='pep2.fasta', type=str,
-                    help='Second peptide fasta file')
-    parser.add_argument("-v", "--verbose", 
-                    help = "increase output verbosity",
-                    action = "store_true")
-    parser.add_argument("-L", "--length", 
-                    type = int,
-                    nargs = '?',
-                    const = 1,
-                    default = 4,
-                    help = "Minimum number of consecutive amino acids above specificity threshold (defined by -S)")
-    parser.add_argument("-s", "--specificity", 
-                    type = str,
-                    nargs = '?',
-                    const = 1,
-                    default = "high", 
-                    help = ("Degree of matching in clustal alignment output." +
-                            "Used with -L to determine minimum length of homologous region.\n" +
-                            "low includes [.:*], medium includes [:*], high includes [*]."))
-    parser.add_argument("-p", "--permutations", 
-                    type = int,
-                    nargs = '?',
-                    const = 1,
-                    default = 100, 
-                    help = ("Number of alignment permutations to run. Default: 100" +
-                            "Used with -L to determine minimum length of homologous region.\n" +
-                            "low includes [.:*], medium includes [:*], high includes [*]."))
+                        help='Second peptide fasta file')
+    parser.add_argument("-d", "--debug", 
+                        help="Assist with debugging by increasing output verbosity",
+                        action="store_true")
+    parser.add_argument("-L", "--length",
+                        type=int,
+                        nargs='?',
+                        const=1,
+                        default=4,
+                        help='''Minimum number of consecutive amino acids above the
+                                specificity threshold (defined by -S)''')
+    parser.add_argument("-s", "--specificity",
+                        type=str,
+                        nargs='?',
+                        const=1,
+                        default="high",
+                        help=("Degree of matching in clustal alignment output." +
+                              "Used with -L to determine minimum length of homologous region.\n" +
+                              "low includes [.:*], medium includes [:*], high includes [*]."))
+    parser.add_argument("-p", "--permutations",
+                        type=int,
+                        nargs='?',
+                        const=1,
+                        default=100, 
+                        help = ("Number of alignment permutations to run. Default: 100" +
+                                "Used with -L to determine minimum length of homologous region.\n" +
+                                "low includes [.:*], medium includes [:*], high includes [*]."))
+
     args = parser.parse_args()
-    if args.verbose:
-        print("verbosity turned on")
+    if args.debug:
+        debug = True
+        print("debug mode: on with increased output verbosity.")
 
     print("""running with length = %s and specificity = %s""" % (args.length, args.specificity))
 
@@ -189,17 +194,25 @@ def main():
     if missing_files > 0:
         sys.exit("Aborting due to missing files")
 
-    concat_alignment = subprocess.check_output(["cat", args.pep1_filename, args.pep2_filename])
+    pep1 = read_text(args.pep1_filename).rstrip() + "\n"    # Ensures file ends with newline
+    pep2 = read_text(args.pep2_filename).rstrip() + "\n"    # Ensures file ends with newline
 
+    #concat_alignment = subprocess.check_output(["cat", args.pep1_filename, args.pep2_filename])
+    concat_alignment = pep1 + pep2
+
+    print(concat_alignment)
     # Read in alignments as text
     # Concatenate within script
     # Feed concatenated fasta file as input via PIPE
 
     pep1 = read_text(args.pep1_filename)
     pep2 = read_text(args.pep2_filename)
+    aln = run_clustal(concat_alignment.encode())
     
-    process = subprocess.run(["./clustalo", "-i", "-", "--outfmt", "clu"], input=concat_alignment, stdout=PIPE)
-    print(process.stdout.decode())
+
+
+    if args.permutations > 0:
+        print("Yes perms")
 
     sys.exit("reached exit")
 
@@ -207,7 +220,9 @@ def main():
 
     # file_list = glob.glob("protein_alignments/*.aln")
     #args = sys.argv
-    args = ["chimera.py", "MET12_MET13.pep.aln", "low" "3", "MET12_flanking.fasta", "MET13_flanking.fasta", "100"]
+    args = ["chimera.py", "MET12_MET13.pep.aln",
+            "low" "3", "MET12_flanking.fasta", 
+            "MET13_flanking.fasta", "100"]
     clustal_aln_file = args[1]
     n_perfect_matches = int(args[2])
     seq_1_plus_1kb_file = args[3]
@@ -262,11 +277,5 @@ def main():
     d2 = get_index_offset(unfolded_alignment[1])
     #c = (  (d1[x],d2[x]) for y in non_homology_regions for x in range(y[0],y[1]))
 
-    c = returnCombos(d1, d2, non_homology_regions)
+    c = return_combos(d1, d2, non_homology_regions)
     print(len(list(c)))
-
-    itertools.permutations(unfolded_alignment[2])
-
-
-if __name__ == "__main__":
-    main()
