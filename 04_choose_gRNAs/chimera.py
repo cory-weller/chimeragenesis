221a10068cf29795b7cb98466e59499e6a755f5a
Cory Weller <cory.a.weller@gmail.com>
2020-08-11 13:53:06 -0400

    update to run optionally with permutations

--- 04_choose_gRNAs/chimera.py
+++ 04_choose_gRNAs/chimera.py
@@ -1,16 +1,19 @@
 #!/usr/bin/env python
 
+####################
+# DEFINE FUNCTIONS #
+####################
+
 def read_text(filename):
     with open(filename, 'r') as infile:
         rawText = infile.read()
     if debug:
         print("Read in text:\n" + rawText)
-    return(rawText)
+    return rawText
 
 def unfold_alignment(rawText):
     text = rawText.splitlines()[3:]
     
-    # iterate through file and unfold text
     lineCount = 0
     seq1 = ''
     seq2 = ''
@@ -41,12 +44,11 @@ def unfold_alignment(rawText):
             new_score += 'f'
         else:
             new_score += score
-    return([seq1, seq2, new_score])
-
+    return [seq1, seq2, new_score]
 
 def get_homology_regions(aln_scores, n_perfect_matches):
     boundaries = [(m.start(0), m.end(0)) for m in re.finditer(r"[\*]{%s,}" % n_perfect_matches, aln_scores)]
-    return(boundaries)
+    return boundaries
 
 def get_overall_score(score):
     gaps = re.findall('[fs]+', score)
@@ -55,28 +57,27 @@ def get_overall_score(score):
     total_gap_length = sum([len(x) for x in gaps])
     total_align_length = len(nonperfect_align) + len(perfect_align)
     overall_score = float(len(perfect_align)) / (len(perfect_align) + len(nonperfect_align) + len(gaps))
-    return([overall_score, total_align_length, total_gap_length, len(perfect_align), len(nonperfect_align), len(gaps)])
+    return [overall_score, total_align_length, total_gap_length, len(perfect_align), len(nonperfect_align), len(gaps)]
 
-# Unnecessary function?
 def pad_from_homology(non_homology_regions, nt_padding):
     non_homology_interior = [ (x[0]+nt_padding, x[1]-nt_padding) for x in non_homology_regions[1:-1]]
     non_homology_first = [(non_homology_regions[0][0], non_homology_regions[0][1]-nt_padding)]
     non_homology_last = [(non_homology_regions[-1][0]+nt_padding, non_homology_regions[-1][1])]
     combined_ranges = non_homology_first + non_homology_interior + non_homology_last
     #return(combined_ranges)
-    return([x for x in combined_ranges if x[0] <= x[1] and x[0] >= 0])
+    return [x for x in combined_ranges if x[0] <= x[1] and x[0] >= 0] 
 
-
-# with open('scores.txt', 'w') as score_file:
-#     score_file.write('\t'.join(['cluster', 'protein1', 'protein2', 'overall_score', 'total_align_length', 'total_gap_length', 'total_perfect_align', 'total_nonperfect_align', 'n_gaps']) + '\n')
-#     for filename in file_list:
-#         cluster, protein1, protein2 = re.split(r"\.|_", filename.split("/")[1])[0:3]
-#         with open(filename + ".2", 'w') as outfile:
-#             seq1, seq2, score = unfold_alignment(filename)
-#             outfile.write('\n'.join([seq1, seq2, score]))
-#             outfile.write('\n')
-#             overall_score, total_align_length, total_gap_length, total_perfect_align, total_nonperfect_align, n_gaps = get_overall_score(score)
-#             score_file.write('\t'.join([str(x) for x in [cluster, protein1, protein2, overall_score, total_align_length, total_gap_length, total_perfect_align, total_nonperfect_align, n_gaps]]) + '\n')
+def write_scores(score_filename):
+    with open(score_filename, 'w') as score_file:
+        score_file.write('\t'.join(['cluster', 'protein1', 'protein2', 'overall_score', 'total_align_length', 'total_gap_length', 'total_perfect_align', 'total_nonperfect_align', 'n_gaps']) + '\n')
+        for filename in file_list:
+            cluster, protein1, protein2 = re.split(r"\.|_", filename.split("/")[1])[0:3]
+            with open(filename + ".2", 'w') as outfile:
+                seq1, seq2, score = unfold_alignment(filename)
+                outfile.write('\n'.join([seq1, seq2, score]))
+                outfile.write('\n')
+                overall_score, total_align_length, total_gap_length, total_perfect_align, total_nonperfect_align, n_gaps = get_overall_score(score)
+                score_file.write('\t'.join([str(x) for x in [cluster, protein1, protein2, overall_score, total_align_length, total_gap_length, total_perfect_align, total_nonperfect_align, n_gaps]]) + '\n')
 
 def open_fasta(filename):
     with open(filename, "r") as infile:
@@ -135,8 +136,16 @@ def run_permutations(n_permutations, pep1, pep2):
             print(result)
         yield result
 
+#######################################################################################################################
+#                                                        MAIN
+#######################################################################################################################
+
 if __name__ == "__main__":
-    print("main!")
+
+    ##################
+    # IMPORT MODULES #
+    ##################
+
     import os
     import sys
     import re
@@ -174,7 +183,6 @@ if __name__ == "__main__":
                         type=int,
                         nargs='?',
                         const=1,
-                        default=100, 
                         help = ("Number of alignment permutations to run. Default: 100" +
                                 "Used with -L to determine minimum length of homologous region.\n" +
                                 "low includes [.:*], medium includes [:*], high includes [*]."))
@@ -187,14 +195,25 @@ if __name__ == "__main__":
 
     print("""running with length = %s and specificity = %s""" % (args.length, args.specificity))
 
-    # VALIDATE ARGUMENTS
+#######################################################################################################################
+
+    ######################
+    # VALIDATE ARGUMENTS #
+    ######################
+
     try: assert args.length > 0, "ERROR: length must be > 0"
     except AssertionError as error: sys.exit(error)
 
     try: assert args.specificity in ["low", "medium", "high"], "specificity must be low, medium, or high"
     except AssertionError as error: sys.exit(error)
 
-    # Check for missing input files
+    
+#######################################################################################################################
+
+    ##################
+    # VALIDATE INPUT #
+    ##################
+
     missing_files = 0
     for filename in [args.pep1_filename, args.pep2_filename]:
         if not os.path.isfile(filename):
@@ -206,30 +225,82 @@ if __name__ == "__main__":
     if missing_files > 0:
         sys.exit("Aborting due to missing files")
 
-    # RUN CODE
+#######################################################################################################################
+
+    ############
+    # RUN CODE #
+    ############
 
     pep1 = read_text(args.pep1_filename).rstrip() + "\n"    # Ensures file ends with newline
     pep2 = read_text(args.pep2_filename).rstrip() + "\n"    # Ensures file ends with newline
 
-    concat_alignment = pep1 + pep2
 
-    aln = run_clustal(concat_alignment.encode())
-    
-    pep1_permuted = permute_peptide_fasta(pep1)
-    pep2_permuted = permute_peptide_fasta(pep2)
-
-
-
-    if args.permutations > 0:
+    if not args.permutations == None:
+        print("Building permutation generator for %s permutations..." % (args.permutations))
         permutations = run_permutations(args.permutations, pep1, pep2)
+        print("Built permutation generator!")
+        print("Writing %s permutations to permutations.out" % (args.permutations))
+        with open("permutations.out", "w") as outfile:
+            current_i = 0
+            for i in permutations:
+                current_i += 1
+                if current_i % 10 == 0:
+                    print(current_i)
+                outfile.write('\n'.join(unfold_alignment(i)) + '\n\n')
+    else:
+        concat_alignment = pep1 + pep2
+        aln = run_clustal(concat_alignment.encode())
 
-    print("Built permutation generator!")
-    #for i in permutations:
-    #    print(i)
 
-    sys.exit("reached exit")
+    sys.exit("Exited successfully!")
 
 
+#######################################################################################################################
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    #####################
+    # DOES NOT RUN CODE #
+    #####################
 
     # file_list = glob.glob("protein_alignments/*.aln")
     #args = sys.argv
@@ -258,7 +329,6 @@ if __name__ == "__main__":
                     [ (homology_regions[x][1], homology_regions[x+1][0]) for x in range(0,len(homology_regions)-1)] +
                     [(homology_regions[-1][1], len(unfolded_alignment[2]))] )
 
-    #print(non_homology_regions)
 
 
     # Iterate through non-homology-regions, and get all combinations of numbers within
@@ -292,3 +362,5 @@ if __name__ == "__main__":
 
     c = return_combos(d1, d2, non_homology_regions)
     print(len(list(c)))
+
+#######################################################################################################################
