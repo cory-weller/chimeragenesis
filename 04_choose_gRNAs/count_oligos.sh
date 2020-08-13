7b6a2b22edf94948f9c3b98991270f577a5f0ed6
Cory Weller <cory.a.weller@gmail.com>
2020-08-13 11:17:37 -0400

    include true alignment output

--- 04_choose_gRNAs/count_oligos.sh
+++ 04_choose_gRNAs/count_oligos.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
+# generate permuted alignment output
 permutations="50000"
-
 for length in $(seq 1 5); do
     for specificity in "low" "medium" "high"; do
         python3 chimera.py -p ${permutations} -s ${specificity} -L ${length}  MET12.pep.fasta MET13.pep.fasta
+    done
+done
+
+# generate true alignment output
+for length in $(seq 1 5); do
+    for specificity in "low" "medium" "high"; do
+        python3 chimera.py -s ${specificity} -L ${length} MET12.pep.fasta MET13.pep.fasta
     done
 done
 
